b1d582f56a7ca42084e90429f246219b23c8948b
ntcxc828 <ntcxc828@gmail.com>
2018-01-16 23:11:06 -0800

    Second commit

--- Tian/src/rpc/SearchItem.java
+++ Tian/src/rpc/SearchItem.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +16,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import db.DBConnection;
+import db.DBConnectionFactory;
 import entity.Item;
 import external.ExternalAPI;
 import external.ExternalAPIFactory;
@@ -41,25 +44,22 @@ public class SearchItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Get parameter from HTTP request
+		String userId = request.getParameter("user_id");
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
-		String term = request.getParameter("term"); // term can be null
-		// call TicketMasterAPI.search to get event data
-		// TicketMasterAPI tmAPI = new TicketMasterAPI();
-		// List<Item> items = tmAPI.search(lat, lon, term);
-		// Connect to external API
-		ExternalAPI api = ExternalAPIFactory.getExternalAPI();
-		List<Item> items = api.search(lat, lon, term);
+		String term = request.getParameter("term"); // Term can be empty or
+													// null.
 
-		// There should be some saveItem logic here
-
-		// Convert Item list back to JSONArray for client
+		DBConnection conn = DBConnectionFactory.getDBConnection();
+		List<Item> items = conn.searchItems(userId, lat, lon, term);
 		List<JSONObject> list = new ArrayList<>();
+		Set<String> favorite = conn.getFavoriteItemIds(userId);
 		try {
 			for (Item item : items) {
-				// Add a thin version of restaurant object
 				JSONObject obj = item.toJSONObject();
+				if (favorite != null) {
+					obj.put("favorite", favorite.contains(item.getItemId()));
+				}
 				list.add(obj);
 			}
 		} catch (Exception e) {
@@ -67,6 +67,7 @@ public class SearchItem extends HttpServlet {
 		}
 		JSONArray array = new JSONArray(list);
 		RpcHelper.writeJsonArray(response, array);
+
 	}
 
 	/**
